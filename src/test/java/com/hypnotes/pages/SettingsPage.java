bd2a97e97a2df2f3620814fa0ea809efad88d8db
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-12-30 14:34:50 +0000

    day 6 103

--- src/test/java/com/hypnotes/pages/SettingsPage.java
+++
@@ -1,9 +0,0 @@
-package com.hypnotes.pages;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class SettingsPage extends CommonPage {
-    @FindBy(css = ".justify-content-start")
-    public WebElement activeSubscriptionYearType;
-}
